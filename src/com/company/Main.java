842759eacc62c028ba7cb6b7d0e99d0161565ba5
jonaswong300 <jonaswong300@hotmail.com>
2020-11-10 22:42:48 +0800

    Pending more test case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        MainController mc = new MainController();
-        //BugReporterUI b = new BugReporterUI("user1");
+        //MainController mc = new MainController();
+        BugReporterUI b = new BugReporterUI("user1");
         //BugDeveloperUI bd = new BugDeveloperUI();
         //TriagerUI t = new TriagerUI();
         //BugReviewerUI br = new BugReviewerUI();
